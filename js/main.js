c15111265a7b45f3b38e0c33e67f3998a52f0616
unknown <velazquezdavo@gmail.com>
2023-08-29 22:43:06 -0600

    ACTUALIZADO

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
-//Esto es un comentario
+//Esto es un comentario > < 
+
+alert("NOTA: En la compra normal, por promoción, tenemos el 5% de descuento, si tu pago es mayor a 10 mil pesos, se te hará un descuento del 20% en tu compra total.");
+
+let pesos = prompt("Ingresa la cantidad total de tu compra: ");
+   let descuento = pesos * .05;
+   let total = pesos - descuento;
+
+alert("El total de tu compra es de " + total);
+
+if(pesos > 10000){
+    descuento = pesos * .20;
+    total = pesos - descuento;
+    alert("El total de tu compra junto con el descuento es de: " + total)
+} 
+
+let experiencia = prompt("Le gustó la experiencia comprando con BEBECA")
+switch (experiencia) {
+    case "Si":
+    alert("Lo esperamos pronto, estamos a su servicio y al de su bebé");
+        break;
+
+    case "No":
+    alert("Lamentamos mucho la experiencia que tuvo, ayudenos a expresar su inconformidad");
+    prompt();
+    alert("Gracias por su honestidad, como empresa buscamos lo mejor para nuestros clientes");
+        break;
+
+    default:
+        break;
+}
+
+
+
 
